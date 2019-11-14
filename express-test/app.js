2ef6d814c143662ad241621e81dd69682bc50bb8
guopeng1129972 <381694179@qq.com>
2019-11-14 23:50:59 +0800

    node

--- express-test/app.js
+++ express-test/app.js
@@ -1,12 +1,17 @@
 //express_demo.js 文件
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
-
-app.get('/', function (req, res) {
-    console.log(req.query)
-    res.send(req.query)
-    // res.send(JSON.parse(req.query) /*stringify(req.query)*/ );
+// 创建 application/x-www-form-urlencoded 编码解析
+var urlencodedParser = bodyParser.urlencoded({
+    extended: false
 })
+
+// app.get('/', function (req, res) {
+//     console.log(req.query)
+//     res.send(req.query)
+// res.send(JSON.parse(req.query) /*stringify(req.query)*/ );
+// })
 // app.get('/index/:id', function (req, res) {
 //     res.send(req.params);
 //     // http://localhost:8081/index/123
@@ -18,8 +23,21 @@ app.get('/index/:id', function (req, res) {
     // {"id":"123"}
     console.log(req.params.id)
     res.json({
-        params: "123"+req.params.id
+        params: "123" + req.params.id
     })
+})
+app.use(express.static('public'));
+app.get('/', function (req, res) {
+    res.send("hello world!");
+})
+app.get('/index', function (req, res) {
+    res.sendFile(__dirname + "/views/" + "index.html");
+
+})
+app.post('/index', urlencodedParser, function (req, res) {
+    var wd = req.body.username;
+    // res.sendFile(__dirname + "/views/" + "index.html");
+    res.redirect("https://www.baidu.com/s?wd=" + wd);
 })
 var server = app.listen(8081, function () {
 
---
+++ miniapp/jest_test_demo/readme.md
@@ -0,0 +1,4 @@
+一个jest的应用
+npm install --save-dev jest
+sudo cnpm install --save-dev jest
+安装jest
