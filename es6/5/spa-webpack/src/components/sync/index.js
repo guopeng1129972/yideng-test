0220a3e46aecd4672b1d2c9bf5f30f0e20cbcb6c
guopeng1129972 <381694179@qq.com>
2019-12-04 00:01:06 +0800

    1

--- es6/5/spa-webpack/src/components/sync/index.js
+++ es6/5/spa-webpack/src/components/sync/index.js
@@ -1,10 +1,12 @@
 import lodash from "lodash-es";
 // import {isArray} from "lodash-es";
-
+import item from './sync.css';
 const  sync =()=>{
     console.log("sync");
+    document.getElementById('app').innerHTML='<h1 class="${item.test}" >hello webpack</h1>';
 }
 const isArray =(args)=>{
+    //这里不能tree-shacking的原因是因为调用里scop
     console.log(isArray)
     // console.log(lodash.isArray(args))
     // console.log(isArray(args))
--- es6/5/spa-webpack/webpack.config.js
+++ es6/5/spa-webpack/webpack.config.js
@@ -1,7 +1,15 @@
 const WebpackDeepScopeAnalysisPlugin = require('webpack-deep-scope-plugin').default;
 
 module.exports = {
+  module:{
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', {
+        loader:'css-loader?modules&localIdentNmae=[name]_[local]=[hash:base64:5]'
+      }/*'css-loader'*/]
+    }]
+  },
   plugins: [
     new WebpackDeepScopeAnalysisPlugin(),
   ],
-}
+};
