cb4928df901773f8220e8f3d5e846395da6554dd
guopeng1129972 <381694179@qq.com>
2019-12-15 23:31:30 +0800

    补充一点简单的面试题

--- mianshi/1/app.js
+++ mianshi/1/app.js
@@ -1,1 +1,14 @@
-console.log(123);
+function a(name) {
+    console.log("this name is"+name);
+}
+
+
+function pro(b, a) {
+    var _prototype = Object.ccreate(a.prototype);
+    _prototype.constructor =a.prototype.constructor;
+    b.prototype=_prototype;
+}
+
+new a1=a('bab');
+new b1;
+pro(b1,a);
