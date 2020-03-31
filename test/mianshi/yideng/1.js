bb8ef9809da7ea32e719cff31bf909d7519910d5
guopeng1129972 <381694179@qq.com>
2020-03-31 23:48:20 +0800

    fanxing

--- test/mianshi/yideng/1.js
+++ test/mianshi/yideng/1.js
@@ -3,6 +3,20 @@ var str = '123hello'
 console.log(typeof (str++));
 console.log(str);
 //2.写出一下打印结果
-['1','3','10'].map(parseInt);
+['1', '3', '10'].map(parseInt);
 
-[1,3,10]
+var a = {
+  n: 1
+};
+var b = a;
+a.x = a = {
+  n: 2
+};
+
+alert(a.x);
+
+
+
+
+
+// --> undefined alert(b.x);// --> {n:2}
