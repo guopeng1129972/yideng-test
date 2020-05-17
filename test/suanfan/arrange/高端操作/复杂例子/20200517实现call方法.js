3d30c5a6b7deb2e51b39323910c7c1915b57c065
guopeng1129972 <381694179@qq.com>
2020-05-18 00:12:29 +0800

    Update 20200517实现call方法.js

--- test/suanfan/arrange/高端操作/复杂例子/20200517实现call方法.js
+++ test/suanfan/arrange/高端操作/复杂例子/20200517实现call方法.js
@@ -1,8 +1,16 @@
+//https://github.com/mqyqingfeng/Blog/issues/11
+/**
+ * 实现call，需要做到这几点
+ * 1.改变this，指向call的参数的函数（得指向谁call的嘛，肯定不能指向干活的）
+ * 2.call的函数要执行了（不然就是没call嘛，指向谁让你干的）
+ * 2.解决循环输入的问题
+ * @param {对象} context 
+ */
 function myCall(context) {
   var context = context | window;
   context.fn = this;
   var arg = [];
-  for (let i = 0; i < arguments.length; i++) {
+  for (let i = 1; i < arguments.length; i++) {
     args.push('arguments[' + i + ']');
 
   }
