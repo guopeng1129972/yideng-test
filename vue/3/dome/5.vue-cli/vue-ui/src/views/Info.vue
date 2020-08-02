73dd9bae6c0faa4eb1d833add0e245ddaadddd3d
guopeng1129972 <381694179@qq.com>
2020-08-02 15:15:14 +0800

    del

---
+++ vue/3/dome/5.vue-cli/vue-ui/src/views/Info.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Hello msg="Welcome to Your info.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import Hello from "@/components/Hello.vue";
+// export default {
+//   name: "Hello",
+//   components: {
+//     Hello,
+//   },
+// };
+</script>
